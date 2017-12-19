2f46dfb3bb83fcca563e77e6a60fe586e60bb669
Quentin Wendegass <quentin.wendegass@gmx.net>
2017-12-19 17:00:51 +0100

    Updated controller software and changed pinout

--- software_controller/controller_client/controller_client.ino
+++ software_controller/controller_client/controller_client.ino
@@ -5,19 +5,16 @@
 
 #include <WebSocketsClient.h>
 
-ESP8266WiFiMulti WiFiMulti;
-WebSocketsClient webSocket;
+#define SELECT 5
+#define START 4
+#define A 9
+#define B 10
+#define UP 15
+#define DOWN 14
+#define LEFT 13
+#define RIGHT 2
 
-#define SELECT 17
-#define START 20
-#define A 19
-#define B 14
-#define UP 13
-#define DOWN 16
-#define LEFT 10
-#define RIGHT 9
-
-#define LED 11
+#define LED 12
 
 #define AP_SSID ""
 #define AP_PASSWORD ""
@@ -28,12 +25,23 @@ bool isPlayerAssigned = false;
 
 bool ledState = false;
 
+bool start_state;
+bool select_state;
+bool a_state;
+bool b_state;
+bool left_state;
+bool right_state;
+bool up_state;
+bool down_state;
+
 int blinkDuration = 0;
 
 long timeLastFrame;
 long timeThisFrame;
 int timer;
 
+ESP8266WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
   
 void setup() {
 
@@ -59,28 +67,28 @@ void setup() {
   pinMode(UP, INPUT);
   pinMode(DOWN, INPUT);
 
-  attachInterrupt(digitalPinToInterrupt(SELECT), select_low, FALLING);
-  attachInterrupt(digitalPinToInterrupt(SELECT), select_high, RISING);
-  attachInterrupt(digitalPinToInterrupt(START), start_low, FALLING);
-  attachInterrupt(digitalPinToInterrupt(START), start_high, RISING);
-  attachInterrupt(digitalPinToInterrupt(A), a_low, FALLING);
-  attachInterrupt(digitalPinToInterrupt(A), a_high, RISING);
-  attachInterrupt(digitalPinToInterrupt(B), b_low, FALLING);
-  attachInterrupt(digitalPinToInterrupt(B), b_high, RISING);
-  attachInterrupt(digitalPinToInterrupt(UP), up_low, FALLING);
-  attachInterrupt(digitalPinToInterrupt(UP), up_high, RISING);
-  attachInterrupt(digitalPinToInterrupt(DOWN), down_low, FALLING);
-  attachInterrupt(digitalPinToInterrupt(DOWN), down_high, RISING);
-  attachInterrupt(digitalPinToInterrupt(LEFT), left_low, FALLING);
-  attachInterrupt(digitalPinToInterrupt(LEFT), left_high, RISING);
-  attachInterrupt(digitalPinToInterrupt(RIGHT), right_low, FALLING);
-  attachInterrupt(digitalPinToInterrupt(RIGHT), right_high, RISING);
+  attachInterrupt(digitalPinToInterrupt(START), start, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(SELECT), select, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(A), a, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(B), b, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(LEFT), left, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(RIGHT), right, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(UP), up, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(DOWN), down, CHANGE);
 
+  start_state = digitalRead(START);
+  select_state = digitalRead(SELECT);
+  a_state = digitalRead(A);
+  b_state = digitalRead(B);
+  left_state = digitalRead(LEFT);
+  right_state = digitalRead(RIGHT);
+  up_state = digitalRead(UP);
+  down_state = digitalRead(DOWN);
+ 
   Serial.printf("Pins intitiallised! \n");
 
   WiFiMulti.addAP(AP_SSID, AP_PASSWORD);
 
-  //WiFi.disconnect();
   while(WiFiMulti.run() != WL_CONNECTED) {
     delay(100);
   }
@@ -109,7 +117,7 @@ void loop() {
     }else{
        blinkDuration = 100;
     }
-
+    
     timeThisFrame = millis();
     int delta = timeThisFrame - timeLastFrame;
     timeLastFrame = timeThisFrame;
@@ -138,96 +146,70 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
       Serial.print("Disconnected!\n");
       isConnected = false;
       break;
-    case WStype_CONNECTED: {
+    case WStype_CONNECTED: 
       Serial.print("Connected\n");
       isConnected = true;
-    }
       break;
     case WStype_TEXT:
       Serial.printf("Message: %s\n", payload);
-      if(payload == (uint8_t*)("player1")){
+      if(strcmp((char *)payload, "player1") == 0){    
         isPlayerAssigned = true;
-      }else if(payload == (uint8_t*)("player2")){
+      }else if(strcmp((char *)payload, "player2") == 0){    
         isPlayerAssigned = true;
-      }else if(payload == (uint8_t*)("noplayer")){
+      }else if(strcmp((char *)payload, "noplayer") == 0){    
         isPlayerAssigned = false;
-      }else if(payload == (uint8_t*)("shutdown")){
-        websocket.disconnect();
+      }else if(strcmp((char *)payload, "shutdown") == 0){
+        webSocket.setReconnectInterval(10000000);
+        webSocket.disconnect();
       }
       break;
   }
 }
 
 void sendMessage(String message){
-    //send message to server
     Serial.print("Send: ");
     Serial.println(message);
     webSocket.sendTXT(message);
 }
 
-void start_high(){
-  sendMessage("startpress");
+void handleState(bool &state, String button){
+ if(state){
+    sendMessage(button);
+  }else{
+    sendMessage(button + "press");
+  }
+  state = !state;
 }
 
-void start_low(){
-  sendMessage("start");
+void start(){
+  handleState(start_state, "start");
 }
 
-void select_high(){
-  sendMessage("selectpress");
+void select(){
+  handleState(select_state, "select");
 }
 
-void select_low(){
-  sendMessage("select");
+void a(){
+  handleState(a_state, "a");
 }
 
-void a_high(){
-  sendMessage("apress");
+void b(){
+  handleState(b_state, "b");
 }
 
-void a_low(){
-  sendMessage("a");
+void left(){
+  handleState(left_state, "left");
 }
 
-void b_high(){
-  sendMessage("bpress");
+void right(){
+  handleState(right_state, "right");
 }
 
-void b_low(){
-  sendMessage("b");
+void up(){
+  handleState(up_state, "up");
 }
 
-void up_high(){
-  sendMessage("uppress");
+void down(){
+  handleState(down_state, "down");
 }
-
-void up_low(){
-  sendMessage("up");  
-}
-
-void down_high(){
-  sendMessage("downpress");
-}
-
-void down_low(){
-  sendMessage("down");
-}
-
-void right_high(){
-  sendMessage("rightpress");
-}
-
-void right_low(){
-  sendMessage("right");
-}
-
-void left_high(){
-  sendMessage("leftpress");
-}
-
-void left_low(){
-  sendMessage("left");
-}
-
- 
 
